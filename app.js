90f63d70c4c29ec3ff40bdefa94c7b8348661783
JEONGSUJONG <jeon95ujong@gmail.com>
2023-12-26 17:51:43 +0900

    conditional & prompt

--- app.js
+++ app.js
@@ -1,102 +1,13 @@
-// const, let Type
-const a = 5;
-const b = 2;
-let myName = "SuJong";
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("Hello " + myName);
-myName = "JSJ";
-console.log("No! My name is " + myName);
+const age = parseInt(prompt("How old are you?"));
 
-// boolean Type
-const isFat = false;
-if (!isFat) {
-    console.log("Go to gym");
+if (isNaN(age) || age < 0) {
+  alert("Please write a real positive number.");
+} else if (age < 18) {
+  alert("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  alert("You can drink.")
+} else if (age > 50 && age <= 80) {
+  alert("You should exercise.")
+} else if (age > 80) {
+  alert("You can do whatever you want.")
 }
-
-// undefined, null 
-let something;
-console.log(something, isFat);
-
-// Array
-const num = [1, 2, 3];
-console.log(num[2]);
-// Array - push
-num.push(true);
-console.log(num);
-// Array - map
-const numbers = [1, 2, 3, 4, 5];
-const multipliedNumbers = numbers.map((number) => number * 2);
-console.log(multipliedNumbers);
-
-// Object
-const player = {
-    name: "sujong",
-    age:  27,
-    points: 10,
-    fat: true
-}
-console.log(player);
-console.log(player.name);
-player.age = 28;
-console.log(player.age);
-player.lastname = " jeong "
-console.log(player.name + player.lastname);
-player.points += 20;
-console.log(player.points);
-
-// function 
-const plus = (a, b) => {
-    console.log(a + b);
-};
-plus(3, 4);
-
-const arrowFunction = (...args) => {
-  console.log(args);
-};
-arrowFunction(1, 2, 3);
-
-function regularFunction() {
-  console.log(arguments);
-}
-regularFunction(1, 2, 3);
-
-const obj = {
-  name: "John",
-  sayHello: () => {
-    console.log("Hello "+ this.name); // this는 obj를 참조하지 않음
-  },
-};
-obj.sayHello();
-
-const obj1 = {
-  name: "John",
-  sayHello: function () {
-    console.log("Hello " + this.name); // this는 obj를 참조
-  },
-};
-obj1.sayHello();
-
-const calcultor = {
-  add: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  div: function (a, b) {
-    console.log(a / b);
-  },
-  multi: function (a, b) {
-    console.log(a * b);
-  },
-  power: function (a, b) {
-    console.log(a ** b);
-  },
-};
-calcultor.add(5, 5)
-calcultor.minus(3, 2);
-calcultor.div(10, 2);
-calcultor.multi(5, 5);
-calcultor.power(2, 5);
