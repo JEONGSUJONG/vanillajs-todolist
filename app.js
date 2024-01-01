796b7e6f6da25c332d0d19952e7f819c0c646f51
JEONGSUJONG <jeon95ujong@gmail.com>
2024-01-01 18:52:03 +0900

    2024.01.01-Vanilla JS

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age) || age < 0) {
-  alert("Please write a real positive number.");
-} else if (age < 18) {
-  alert("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  alert("You can drink.")
-} else if (age > 50 && age <= 80) {
-  alert("You should exercise.")
-} else if (age > 80) {
-  alert("You can do whatever you want.")
-}
+const title = document.querySelector(".hello h1");
+title.innerText = "Hello!"
+console.dir(title);
