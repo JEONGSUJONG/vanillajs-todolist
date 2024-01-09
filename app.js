2213ba13e5abbe08c9527de3a5ae7958d6e74654
JEONGSUJONG <jeon95ujong@gmail.com>
2024-01-09 14:15:27 +0900

    addEventListener

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-const title = document.querySelector(".hello h1");
-title.innerText = "Hello!"
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  h1.innerText = "Mouse was clicked";
+}
+function handleMouseEnter() {
+  h1.innerText = "Mouse is here";
+}
+function handleMouseLeave() {
+  h1.innerText = "Bye";
+}
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "blue";
+}
+function handleWindowCopy() {
+  alert("copier!");
+}
+function handleWindowOffline() {
+  alert("No Wifi");
+}
+function handleWindowOnline() {
+  alert("Connected");
+}
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
